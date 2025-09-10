8eca66a83d7284b47ce4dbe31b7abc0363f178c7
Kehinde Adeoso <kennolite@gmail.com>
2025-09-10 12:32:39 -0400

    Implemented header portion of the GBBus struct.

--- GameboyContext.cpp
+++ GameboyContext.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "GameboyContext.h"
+
+int GBBus::WorkRamBankD() const {
+        if (!GBC) return 1;
+        const BYTE b = m_SVBK & 0x07;
+
+        return b ? b : 1;
+}
+
+
+uint8_t GBBus::read8(uint16_t addr) {
+}
+
+void GBBus::write8(uint16_t addr, uint8_t data) {
+
+}
--- GameboyContext.h
+++ GameboyContext.h
@@ -7,6 +7,7 @@
 
 #include <array>
 #include <cstdint>
+#include <stack>
 
 struct Cartridge; // ROM Banking, External RAM, Memory Bank Controller registers
 struct PPU; // Picture Processing Unit, owns VRAM, OAM, PPU regs
@@ -17,7 +18,10 @@ struct DMA; // Direct Memory Access
 struct Interrupts; // Interrupt Flag and Interrupt Enable | IF (FF0F) + IE (FFFF)
 
 
-struct Bus {
+struct GBBus {
+    using BYTE = u_int8_t;
+    using WORD = u_int16_t;
+
     // Devices
     Cartridge* cartridge;
     PPU* ppu;
@@ -27,19 +31,30 @@ struct Bus {
     DMA* dma;
     Interrupts* interrupts;
 
-    // Memory
-    bool bootrom_mapped = true;
-    std::array<uint8_t, 0x100> bootrom{};
-    std::array<uint8_t, 8 * 1024> bus_wram_bank0{};
-    std::array<uint8_t, 4 * 1024> bus_wram_bank1{}; // banked on GBC
-    std::array<uint8_t, 127> bus_hram{};
-    uint8_t io[0x80]{}; // or route each IO addr to owner
+    BYTE m_WorkRam[8][0x1000];
+    BYTE m_HighRam[0x7];
+    BYTE m_ObjectAttributeMemory[0xA0];
+    BYTE m_SVBK= 1; // WRAM Bank Select. Selects which RAM banks to use for GBC.
+    int GBC = 0; // Set to 1 if emulating GBC, 0 otherwise
+
+    inline int WorkRamBankD() const;
+
 
     uint8_t read8(uint16_t addr);
     void    write8(uint16_t addr, uint8_t data);
+};
 
+struct CPU {
+    uint8_t A,F,B,C,D,E,H,L; // Must they really be separate?
+    uint16_t PC, SP;
 
+    /* Might be useful for later.
+    inline void setFlagZ(bool v){ F = v ? (F|0x80) : (F&~0x80); }
+    // N=0x40, H=0x20, C=0x10; low nibble must stay 0 on writes
+    inline void writeF(uint8_t v){ F = v & 0xF0; }
+    */
 
+    int step(class Bus& bus); // fetch/decode/execute, returns cycles
 };
 
 
